f6a5147e8d88ca1663824844fbf8f15f57657a65
Yokeshwaran-K <yokeshwaranyoki001@gmail.com>
2026-07-03 17:29:16 +0530

    Update Header.tsx

--- sovereign/app/components/Header.tsx
+++ sovereign/app/components/Header.tsx
@@ -4,40 +4,9 @@ import Image from "next/image";
 export default function Header() {
   return (
     <header className="shadow-md">
-      <div className="container mx-auto px-4">
-        <div className="flex items-center justify-between py-4">
-
-          <Link href="/">
-            <Image
-              src="/images/SovereignHouse-logo.png"
-              alt="Sovereign House"
-              width={120}
-              height={120}
-            />
-          </Link>
-
-          <nav>
-            <ul className="flex gap-6">
-              <li><Link href="/selling">Sales</Link></li>
-              <li><Link href="/lettings">Lettings</Link></li>
-              <li><Link href="/about-us">About Us</Link></li>
-              <li><Link href="/our-services">Our Services</Link></li>
-              <li><Link href="/register">Register</Link></li>
-              <li><Link href="/property-news">News</Link></li>
-              <li><Link href="/contact-us">Contact</Link></li>
-            </ul>
-          </nav>
-
-          <div className="flex gap-3">
-            <a href="tel:02089855800">
-              Call Us
-            </a>
-
-            <button>
-              Valuation
-            </button>
-          </div>
-
+      <div className="flex">
+        <div className="header-menu">
+          <Image src="/images"/>
         </div>
       </div>
     </header>
